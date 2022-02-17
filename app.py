12b5e01c9ef9a3d05e363db755e01bf2ff1aa642
gaiusman <gods1209@gmail.com>
2022-02-17 19:51:34 +0900

    ds

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.koreaboardgames.com/boardgame/game_list01.php#pagenum',headers=headers)
+app = Flask(__name__)
 
-soup = BeautifulSoup(data.text, 'html.parser')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-trs = soup.select('#gameList > li:nth-child(1)')
-
-for tr in trs:
-    title = tr.select_one('div.desc > p > a').text
-    psn = tr.select_one('table > tbody > tr:nth-child(2) > td').text
-    age = tr.select_one('table > tbody > tr:nth-child(3) > td').text
-    time = tr.select_one('table > tbody > tr:nth-child(4) > td').text
-    print(title)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
