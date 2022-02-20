856f095d21fed03a26992f798033298851fbfe21
gaiusman <gods1209@gmail.com>
2022-02-20 23:41:15 +0900

    ds

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, jsonify, request
 
 from pymongo import MongoClient
 
-# client = MongoClient('mongodb://test:test@localhost', 27017)
+#client = MongoClient('mongodb://test:test@localhost', 27017)
 client = MongoClient('localhost', 27017)
 db = client.mc10th
 
 app = Flask(__name__)
 
-# 메인 페이지
+#메인 페이지
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 질문 페이지
+#질문 페이지
 @app.route('/survey')
 def survey():
     return render_template('question.html')
@@ -22,6 +22,7 @@ def survey():
 # 질답에 체크한 데이터를 DB_survey에 저장하고
 # DB_gameList와 비교하여
 # 일치하는 데이터들을 뽑아내는 기능
+
 @app.route('/survey', methods=['POST'])
 def search_survey():
     num_receive = request.form['num_give']
@@ -29,33 +30,39 @@ def search_survey():
     genre_receive = request.form['genre_give']
     time_receive = request.form['time_give']
 
-    doc = {
-        'sur_num': num_receive,
-        'sur_age': age_receive,
-        'sur_genre': genre_receive,
-        'sur_time': time_receive
-    }
-    db.survey.insert_one(doc)
-    return jsonify({'msg': '저장되었습니다!'})
+    # 원하는 플레이타임, 장르, 연령, 인원으로 원하는 값 출력
+    time = time_receive  # 플레이타임리스트
+    genre = genre_receive # 장르리스트
+    age = age_receive  # 연령리스트
+    num = num_receive  # 최소인원리스트
+    choice = {"opt_time": time, "opt_genre": genre, "opt_age": age, "opt_minNum": num}  # 조건
+    choicegame = db.gameList.find(choice, {'_id': False})  # 검색
+    for mygames in choicegame:  # 반복문
+        print(mygames) # 필터링 할 값 받아옴. 한 개의 값만 받아오는게 안됨.
+    return jsonify({mygames}) #코드 작성하다가 잘 모르겠음.
+
+#질문페이지 게임보기 API
+@app.route('/survey', methods=['GET'])
+def view_games():
+    games = list(db.gameList.find({}, {'_id': False}))
+    return jsonify({games})
 
 # 결과 페이지
-@app.route('/result')
-def result():
-    return render_template('result.html')
-
+# @app.route('/result')
+# def result():
+#     return render_template('result.html')
+#
 # 전체 리스트 페이지
 @app.route('/whole')
 def whole():
     return render_template('full_list.html')
-
-# 전체 리스트 페이지 API
-# DB_gameList의 데이터를 불러와 출력하기
-@app.route('/whole', methods=['GET'])
-def show_gameList():
-    games = list(db.mc10th.find({}, {'_id':False}))
-    return jsonify({'all_games': games})
-
-
+#
+# # 전체 리스트 페이지 API
+# # DB_gameList의 데이터를 불러와 출력하기
+# @app.route('/whole', methods=['GET'])
+# def show_gameList():
+#     games = list(db.mc10th.find({}, {'_id':False}))
+#     return jsonify({'all_games': games})
 
 
 if __name__ == '__main__':
--- test_random.py
+++ test_random.py
@@ -1,7 +1,84 @@
-import random
+from pymongo import MongoClient
 
-def randomNum():
-    a = random.randrange(1, 101)
-    print(a)
+client = MongoClient('localhost', 27017)
+db = client.mc10th
 
-randomNum()
+# name = db.gameList.find_one({"opt_name":"할리갈리"})
+# print(name)
+#
+# results = db.gameList.find({'opt_rank': {$lte:7}})
+#
+# for result in results:
+#     print(result['opt_name'])
+
+# 전체리스트
+# for games in db.gameList.find():
+#     print(games)
+
+# 일부리스트
+# for games in db.gameList.find({},{"_id":0, "opt_name":1, "opt_rank":1}):
+#     print(games)
+
+# 검색
+# myquery = {"opt_name" : "우봉고"} #조건
+# mygame = db.gameList.find(myquery) #검색
+# for games in mygame: #반복문
+#     print(games) #도출값
+
+# 랜덤으로 숫자를 뽑아보자
+# import random
+# min = random.randint(1,8)
+# print(min)
+
+# 인원 검색리스트(랜덤)
+# import random
+# personnel = random.randint(1,8)  #1~8명까지 랜덤으로 한 숫자를 선택(인원 조건을 바꿔줌)
+# myquery = {"opt_minNum" : personnel} #조건
+# mygame = db.gameList.find(myquery) #검색
+# for games1 in mygame: #반복문
+#     print(games1) #결과값
+
+# # 인원 전체리스트
+# personnel = 2  # 2명인 인원 전체 리스트
+# myquery = {"opt_minNum": personnel}  # 조건
+# mygame = db.gameList.find(myquery,{'_id': False})  # 검색
+# for games1 in mygame:  # 반복문
+#     print(games1) #결과값
+
+# 원하는 게임 연령리스트(랜덤)
+# import random
+# age = random.randint(3,10)  #3~10세까지 랜덤으로 한 숫자를 선택(나이 조건을 바꿔줌)
+# myquery = {"opt_age" : age} #조건
+# mygame = db.gameList.find(myquery,{'_id': False}) #검색
+# for games2 in mygame: #반복문
+#     print(games2) #결과값
+
+# 원하는 게임 연령리스트
+# age = 8  # 8세인 전체 리스트
+# myquery = {"opt_age": age}  # 조건
+# mygame = db.gameList.find(myquery,{'_id': False})  # 검색
+# for games2 in mygame:  # 반복문
+#   print(games2) #결과값
+
+# 원하는 장르
+# genre = "전략"  # 장르가 전략인 전체 리스트
+# myquery = {"opt_genre": genre}  # 조건
+# mygame = db.gameList.find(myquery,{'_id': False})  # 검색
+# for games3 in mygame:  # 반복문
+#   print(games3) #결과값
+
+# 원하는 플레이타임, 장르, 연령, 인원으로 원하는 값 출력
+time = 30  #플레이타임이 30분인 리스트
+age = 8 #8세 연령리스트
+personnel = 3 #최소인원이 3인 리스트
+choice = {"opt_time": time, "opt_genre": "전략", "opt_age": age, "opt_minNum": personnel}  # 조건
+choicegame = db.gameList.find(choice,{'_id': False})  # 검색
+for games4 in choicegame:  # 반복문
+
+    print([games4]) #결과값
+    # print(games4['opt_name'])  # 이름 결과값
+    #중복되는 이름에서 그 이름에 해당하는 딕셔너리 값을 뽑아오면 될 듯.
+
+# 선택한 값 도출
+# def find_keys(dict, val):
+#   return list(key for key, value in dict.items() if value == val)
