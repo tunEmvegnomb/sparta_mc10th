369f7d4be64dcbf979ba2a282c53d131f8ae8ff9
gaiusman <gods1209@gmail.com>
2022-02-21 17:33:01 +0900

    ds

--- test_db.py
+++ test_db.py
@@ -68,17 +68,27 @@ db = client.mc10th
 #   print(games3) #결과값
 
 # 원하는 플레이타임, 장르, 연령, 인원으로 원하는 값 출력
-time = 30  #플레이타임이 30분인 리스트
-age = 8 #8세 연령리스트
-personnel = 3 #최소인원이 3인 리스트
-choice = {"opt_time": time, "opt_genre": "전략", "opt_age": age, "opt_minNum": personnel}  # 조건
+for games3 in choice
+    choice =
+time = 60  #플레이타임이 30분인 리스트
+genre = "전략"  # 장르가 전략인 전체 리스트
+age = 10 #8세 연령리스트
+personnel = 2 #최소인원이 3인 리스트
+choice = {"opt_time": time, "opt_genre": genre, "opt_age": age, "opt_minNum": personnel}  # 조건
 choicegame = db.gameList.find(choice,{'_id': False})  # 검색
 for games4 in choicegame:  # 반복문
-
-    print([games4]) #결과값
+    print(games4) #결과값
     # print(games4['opt_name'])  # 이름 결과값
     #중복되는 이름에서 그 이름에 해당하는 딕셔너리 값을 뽑아오면 될 듯.
+
+
 
 # 선택한 값 도출
 # def find_keys(dict, val):
 #   return list(key for key, value in dict.items() if value == val)
+
+#조건문
+# if age <= 8:
+#     print("어린이 입니다")
+# else:
+#     print("어린이가 아닙니다")
