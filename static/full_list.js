6b85c1a2b9a7fee1d9b5416ebc0366768d612e86
chocholove <gmerit0116@naver.com>
2022-02-23 01:38:01 +0900

    sidebar filter

--- static/full_list.js
+++ static/full_list.js
@@ -21,15 +21,14 @@
 //
 // });
 // 모달창 on/off 함수
-      function handleModal() {
-        let status = $("#modal").css("display");
-        if (status === "none") {
-          $("#modal").css("display", "flex");
-        } else {
-          $("#modal").hide();
-        }
-      }
-
+function handleModal() {
+    let status = $("#modal").css("display");
+    if (status === "none") {
+        $("#modal").css("display", "flex");
+    } else {
+        $("#modal").hide();
+    }
+}
 
 
 // For Filters
@@ -116,3 +115,50 @@ document.addEventListener("DOMContentLoaded", function () {
         thumbRight.classList.remove("active");
     });
 });
+
+//태그
+$('.md-select').on('click', function () {
+    $(this).toggleClass('active')
+})
+
+$('.md-select ul li').on('click', function () {
+    var v = $(this).text();
+    $('.md-select ul li').not($(this)).removeClass('active');
+    $(this).addClass('active');
+    $('.md-select label button').text(v)
+})
+
+
+//range
+window.onload = function(){
+    slideOne();
+    slideTwo();
+}
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+
+function slideOne(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderOne.value = parseInt(sliderTwo.value) - minGap;
+    }
+    displayValOne.textContent = sliderOne.value;
+    fillColor();
+}
+function slideTwo(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderTwo.value = parseInt(sliderOne.value) + minGap;
+    }
+    displayValTwo.textContent = sliderTwo.value;
+    fillColor();
+}
+function fillColor(){
+    percent1 = (sliderOne.value / sliderMaxValue) * 100;
+    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #11698e ${percent1}% , #11698e ${percent2}%, #dadae5 ${percent2}%)`;
+}
