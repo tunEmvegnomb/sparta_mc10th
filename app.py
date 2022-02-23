964dec205c9ace549ba64c670ffe39af703f0d55
이승준 <lsj135779@gmail.com>
2022-02-23 23:08:49 +0900

    add link in modal

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ def filter_whole():
     return jsonify({'filtered_games': filtered_games})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
